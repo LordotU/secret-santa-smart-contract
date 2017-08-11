53f8fc12316e6c7c772111e297664c2c4ec6fe05
LordotU <LevshinO@gmail.com>
2017-08-11 13:43:44 +0300

    Improvement working with configs

--- truffle.js
+++ truffle.js
@@ -1,10 +1,47 @@
-module.exports = {
-  networks: {
-    development: {
-      host: "localhost",
-      port: 8646,
-      network_id: "*", // Match any network id
-	  from : "0xe8b09dcf222d1f05b2fc3845de0df3318bb30cdb",
-    }
-  }
-};
+const fs             = require('fs');
+const merge          = require('lodash.merge');
+const configDefaults = require('./truffle.js.default');
+
+
+/**
+ * [readConfigDir description]
+ * @param  {[type]} configDir [description]
+ * @return {[type]}           [description]
+ */
+const readConfigDir = configDir => (
+	fs.readdirSync(`./config/${configDir}`)
+		.filter ( file => /\.js/.test(file) )
+		.map    ( file => file.replace(/\.js$/i, '') )
+);
+
+
+/**
+ * [buildConfigPart description]
+ * @param  {[type]} configType         [description]
+ * @param  {[type]} configTypeDefaults [description]
+ * @return {[type]}                    [description]
+ */
+const buildConfigPart = (configType, configTypeDefaults) => (
+	readConfigDir(configType).reduce(
+		(configPart, key) => {
+			configPart[configType][key] = merge(
+				{},
+				configTypeDefaults,
+				require(`./config/${configType}/${key}`)
+			);
+			return configPart;
+		},
+		{
+			[configType] : {}
+		}
+	)
+);
+
+
+const configAssigned = merge(
+	{},
+	configDefaults,
+	buildConfigPart('networks', configDefaults.rpc),
+);
+
+module.exports = configAssigned;
